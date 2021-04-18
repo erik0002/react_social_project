a561928f8f6ab0b765265e7dba4790114bf64af3
Yeliazar <erik.ruzhakov@mail.ru>
2021-04-18 22:48:31 +0300

    UsersApi

--- src/redux/profile-reducer.js
+++ src/redux/profile-reducer.js
@@ -1,6 +1,7 @@
 /* eslint-disable default-case */
 const ADD_POST = 'ADD-POST';
 const UPDATE_NEW_POST_TEXT = 'UPDATE-NEW-POST-TEXT';
+const SET_USER_PROFILE = 'SET-USER-PROFILE';
 
 let initialState = {
     PostData: [
@@ -8,7 +9,8 @@ let initialState = {
         {id: 2, message: "it's my first post", likeCounts: 11},
         {id: 3, message: "it's my second post", likeCounts: 21}
         ], 
-    newPostText: 'Hi! I\'m happy that you here'
+    newPostText: 'Hi! I\'m happy that you here', 
+    profile: null
 };
 
 const profileReducer = (state = initialState, action) => {
@@ -31,6 +33,12 @@ const profileReducer = (state = initialState, action) => {
                 newPostText: action.newText
             };
         }
+        case SET_USER_PROFILE: {
+            return {
+                ...state, 
+                profile: action.profile
+            };
+        }
         default: 
             return state;
     }
@@ -40,5 +48,9 @@ export const addPostActionCreator = () => ({ type: ADD_POST })
 
 export const updateNewPostTextActionCreator = (text) =>
  ({ type: UPDATE_NEW_POST_TEXT, newText: text})
+
+export const setUserProfile = (profile) => ({
+    type: SET_USER_PROFILE, profile
+})
 
 export default profileReducer;
